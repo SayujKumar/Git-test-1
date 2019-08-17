77dd8287ee92202600d96b58ddb46875afa1c47e
SayujKumar <45237326+SayujKumar@users.noreply.github.com>
2019-08-17 11:38:07 +0530

    Update Feature.cs
    modified add method

--- MYTestApp/MYTestApp/Feature.cs
+++ MYTestApp/MYTestApp/Feature.cs
@@ -8,9 +8,12 @@ namespace MYTestApp
 {
     class Feature
     {
-        public int Add(int x1,int x2)
+        public int Add()
         {
-            return x1 + x2;
+            int x1 = 10;
+            int x2 = 20;
+            var sum = x1 + x2;
+            return sum;
         }
     }
 }
