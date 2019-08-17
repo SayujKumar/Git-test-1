9b4e329495c078ec4401c4c2e0d3fa86557d980b
Sayuj Pournami <sayuj22@gmail.com>
2019-08-17 11:22:35 +0530

    add new method sum

--- MYTestApp/MYTestApp/Feature.cs
+++ MYTestApp/MYTestApp/Feature.cs
@@ -8,5 +8,12 @@ namespace MYTestApp
 {
     class Feature
     {
+        public int Add()
+        {
+            int x1 = 10;
+            int x2 = 20;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
